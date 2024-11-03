624b549b2ced14cf7fb4460e7f4ca48337136106
7minutech <elijohnson1107@gmail.com>
2024-11-03 16:24:03 -0600

    refactor: node and graph class
    Graph will have list of nodes and node will each
    have a piece

---
+++ lib/graph.rb
@@ -0,0 +1,5 @@
+class Graph
+  def initialize
+    nodes = []
+  end
+end
--- lib/node.rb
+++ lib/node.rb
@@ -2,5 +2,6 @@ class Node
   def initialize(square_label)
     @square_label = square_label
     @links = []
+    @piece = nil
   end
 end
