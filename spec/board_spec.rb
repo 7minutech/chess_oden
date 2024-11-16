e186071391d73d825cd20555818cdeb8dc9f757b
7minutech <elijohnson1107@gmail.com>
2024-11-16 14:03:43 -0600

    refactor: replaced display test
    New test displays board after calling fill_board
    making the old test no longer needed

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,17 +3,6 @@ describe Board do
   let(:chess_board) { described_class.new }
   describe "#display_baord" do
     before do
-      (0..7).each do |file|
-        chess_board.board[0][file] = "\u265A"
-        chess_board.board[1][file] = "\u265F"
-        chess_board.board[6][file] = "\u2659"
-        chess_board.board[7][file] = "\u2654"
-      end
-    end
-    it "prints chess board" do
-      chess_board.send(:display_board)
-    end
-    before do
       chess_board.fill_board
     end
     context "when board is filled" do
