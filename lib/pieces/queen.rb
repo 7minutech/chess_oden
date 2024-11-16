b96b670d630dd4c5d511354a9be5f6d5019fb320
7minutech <elijohnson1107@gmail.com>
2024-11-16 13:00:07 -0600

    feat: added starting_range? method
    Needed for fill_board method to place queens
    correctly upon init

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,4 +1,12 @@
 class Queen
   def initialize
   end
+
+  def self.starting_range?(row, col)
+    starting_rows = [0, 7]
+    starting_cols = [3]
+    return true if starting_rows.include?(row) && starting_cols.include?(col)
+
+    false
+  end
 end
