127345ea5588bd03c3d51827074fa2d4676ce58a
7minutech <elijohnson1107@gmail.com>
2024-11-16 13:02:35 -0600

    feat: added starting_range? method
    Needed for fill board method to correctly place
    kings

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,4 +1,12 @@
 class King
   def initialize
   end
+
+  def self.starting_range?(row, col)
+    starting_rows = [0, 7]
+    starting_cols = [4]
+    return true if starting_rows.include?(row) && starting_cols.include?(col)
+
+    false
+  end
 end
