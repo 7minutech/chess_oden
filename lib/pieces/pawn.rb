24c405d429d163b90b6156e7244e1c00bd4aacf8
7minutech <elijohnson1107@gmail.com>
2024-11-16 13:05:11 -0600

    feat: added starting_range? method
    Needed for fill_board method to correctly place
    pawns

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,4 +1,12 @@
 class Pawn
   def initialize
   end
+
+  def self.starting_range?(row, col)
+    starting_rows = [1, 6]
+    starting_cols = (0..7)
+    return true if starting_rows.include?(row) && starting_cols.include?(col)
+
+    false
+  end
 end
