61e6d6a779048524a6332b7e0b899bb401acf5b4
7minutech <elijohnson1107@gmail.com>
2024-11-16 11:59:04 -0600

    feat: added test for fill board method
    Needed to test to ensure that the board is setup
    correctly upon init

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,4 +14,32 @@ describe Board do
       chess_board.send(:display_board)
     end
   end
+  describe "#fill_board" do
+    before do
+      chess_board.send(:fill_board)
+    end
+    it "fills board with chess pieces" do
+      expected_positions = {
+        # White major pieces
+        [0, 0] => Rook, [0, 1] => Knight, [0, 2] => Bishop, [0, 3] => Queen, [0, 4] => King,
+        [0, 5] => Bishop, [0, 6] => Knight, [0, 7] => Rook,
+
+        # Black major pieces
+        [7, 0] => Rook, [7, 1] => Knight, [7, 2] => Bishop, [7, 3] => Queen, [7, 4] => King,
+        [7, 5] => Bishop, [7, 6] => Knight, [7, 7] => Rook,
+
+        # White pawns
+        [1, 0] => Pawn, [1, 1] => Pawn, [1, 2] => Pawn, [1, 3] => Pawn,
+        [1, 4] => Pawn, [1, 5] => Pawn, [1, 6] => Pawn, [1, 7] => Pawn,
+
+        # Black pawns
+        [6, 0] => Pawn, [6, 1] => Pawn, [6, 2] => Pawn, [6, 3] => Pawn,
+        [6, 4] => Pawn, [6, 5] => Pawn, [6, 6] => Pawn, [6, 7] => Pawn
+      }
+
+      expected_positions.each do |(row, col), piece_class|
+        expect(chess_board.board[row][col]).to be_a(piece_class)
+      end
+    end
+  end
 end
