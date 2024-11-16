9ee2ed3b5bb9dc81de0a862e4093e174214c1beb
7minutech <elijohnson1107@gmail.com>
2024-11-16 12:44:14 -0600

    refactor: changed starting_range class method
    For simplification of fill board method it is
    better to have a class method that returns true
    if the given row and col are the starting squares

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -2,7 +2,11 @@ class Rook
   def initialize
   end
 
-  def self.starting_range
-    [0, 7]
+  def self.starting_range?(row, col)
+    starting_rows = [0, 7]
+    starting_cols = [0, 7]
+    return true if starting_rows.include?(row) && starting_cols.include?(col)
+
+    false
   end
 end
