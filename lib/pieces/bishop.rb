5c4309abe0d6936ea7e07f6d1711150c55449af3
7minutech <elijohnson1107@gmail.com>
2024-11-16 12:57:28 -0600

    feat: added starting_range? method
    Needed for fill_board method so bishop are placed
    correctly upon init

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,4 +1,12 @@
 class Bishop
   def initialize
   end
+
+  def self.starting_range?(row, col)
+    starting_rows = [0, 7]
+    starting_cols = [2, 5]
+    return true if starting_rows.include?(row) && starting_cols.include?(col)
+
+    false
+  end
 end
