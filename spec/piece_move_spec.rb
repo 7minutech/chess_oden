7b7c0b75bba7436c642ae6762ccdbb3ac7ce0597
7minutech <elijohnson1107@gmail.com>
2024-11-16 14:53:23 -0600

    feat: added test for new class
    Need to check if the current square each piece
    starts at is correct

---
+++ spec/piece_move_spec.rb
@@ -0,0 +1,21 @@
+require_relative "../lib/piece_move"
+
+describe PieceMove do
+  let(:piece_move) { described_class.new }
+  describe "#piece_current_sqaure" do
+    before do
+      board = piece_move.board
+      board.fill_board
+    end
+    it "sets the current squares" do
+      staring_rows = [0, 1, 6, 7]
+      board.each_with_index do |row, row_index|
+        row.each_index do |col_index|
+          if staring_rows.include?(row_index)
+            expect(board[row_index][col_index]).current_square.to eq([row_index, col_index])
+          end
+        end
+      end
+    end
+  end
+end
