13d2f2e6fe78da01e153d0f1c7b09b9216ecd3a4
7minutech <elijohnson1107@gmail.com>
2024-11-17 17:42:20 -0600

    fix: removed calls to convert method
    Need to remove or test expect an Array instead of
    Pawn object

--- spec/piece_move_spec.rb
+++ spec/piece_move_spec.rb
@@ -18,14 +18,10 @@ describe PieceMove do
       it "sets the correct possibles for pawns" do
         board = piece_move.board_obj.board
         piece_move.pawn_moves
-        a2 = PieceMove.convert_chess_notation("a2")
-        a7 = PieceMove.convert_chess_notation("a7")
-        b2 = PieceMove.convert_chess_notation("b2")
-        b7 = PieceMove.convert_chess_notation("b7")
-        a2 = board[a2[0], a2[1]]
-        a7 = board[a7[0], a7[1]]
-        b2 = board[b2[0], b2[1]]
-        b7 = board[b7[0], b7[1]]
+        a2 = board[6][0]
+        a7 = board[1][0]
+        b2 = board[6][1]
+        b7 = board[1][1]
         expect(a2.possible_moves).to include([5, 0], [4, 0])
         expect(a7.possible_moves).to include([2, 0], [3, 0])
         expect(b2.possible_moves).to include([5, 1], [4, 1])
