e5bbe954237da3a5c1effa98c356fbca9bdf8008
7minutech <elijohnson1107@gmail.com>
2024-11-17 17:20:17 -0600

    feat: added test to convert chess notation
    Need to convert a move like a1 to its actual
    row and col on the board

--- spec/piece_move_spec.rb
+++ spec/piece_move_spec.rb
@@ -3,6 +3,16 @@ require "pry-byebug"
 
 describe PieceMove do
   let(:piece_move) { described_class.new }
+  describe "#convert_chess_notation" do
+    it "takes a chess notation and converts it to an corresponding array" do
+      a2 = PieceMove.convert_chess_notation("a2")
+      d4 = PieceMove.convert_chess_notation("d4")
+      f6 = PieceMove.convert_chess_notation("f6")
+      expect(a2).to eq([6, 1])
+      expect(d4).to eq([4, 3])
+      expect(f6).to eq([2, 5])
+    end
+  end
   describe "#pawn_moves" do
     context "at the start of the game" do
       it "sets the correct possibles for pawns" do
