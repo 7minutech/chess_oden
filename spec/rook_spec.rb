595f96d15db0c6984255ff1f6c9d73b8648c53aa
7minutech <elijohnson1107@gmail.com>
2024-11-18 17:01:58 -0600

    fix: typo

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -20,9 +20,9 @@ describe Rook do
       it "creates list of possible moves" do
         start_black_rook_a8.create_possible_moves
         start_black_rook_h8.create_possible_moves
-        expect(start_white_rook_a8.possible_moves).to include([0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
+        expect(start_black_rook_a8.possible_moves).to include([0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
                                                               [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0])
-        expect(start_white_rook_h8.possible_moves).to include([0, 6], [0, 5], [0, 4], [0, 3], [0, 2], [0, 1], [0, 0],
+        expect(start_black_rook_h8.possible_moves).to include([0, 6], [0, 5], [0, 4], [0, 3], [0, 2], [0, 1], [0, 0],
                                                               [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7])
       end
     end
