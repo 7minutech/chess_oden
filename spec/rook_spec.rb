6994ada8e9ed5c061a47979bb4c63b4ecb89c143
7minutech <elijohnson1107@gmail.com>
2024-11-18 16:51:04 -0600

    fix: updated possible move expects
    Didn't update move for rooks

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -8,8 +8,10 @@ describe Rook do
       it "creates list of possible moves" do
         start_white_rook_a1.create_possible_moves
         start_white_rook_h1.create_possible_moves
-        expect(start_white_rook_a1.possible_moves).to include([5, 0], [4, 0], [5, 1])
-        expect(start_white_rook_h1.possible_moves).to include([5, 3], [4, 3], [5, 2], [5, 4])
+        expect(start_white_rook_a1.possible_moves).to include([7, 1], [7, 2], [7, 3], [7, 4], [7, 5], [7, 6], [7, 7],
+                                                              [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0])
+        expect(start_white_rook_h1.possible_moves).to include([7, 6], [7, 5], [7, 4], [7, 3], [7, 2], [7, 1], [7, 0],
+                                                              [6, 7], [5, 7], [4, 7], [3, 7], [2, 7], [1, 7], [0, 7])
       end
     end
     context "at the start of the game for black" do
@@ -18,8 +20,10 @@ describe Rook do
       it "creates list of possible moves" do
         start_black_rook_a8.create_possible_moves
         start_black_rook_h8.create_possible_moves
-        expect(start_black_rook_a8.possible_moves).to include([2, 0], [3, 0], [2, 1])
-        expect(start_black_rook_h8.possible_moves).to include([2, 3], [3, 3], [2, 2], [2, 4])
+        expect(start_white_rook_a8.possible_moves).to include([0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
+                                                              [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0])
+        expect(start_white_rook_h8.possible_moves).to include([0, 6], [0, 5], [0, 4], [0, 3], [0, 2], [0, 1], [0, 0],
+                                                              [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7])
       end
     end
   end
