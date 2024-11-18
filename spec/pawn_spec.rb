2b7a56305775e980b3dde723043177d0adfa050d
7minutech <elijohnson1107@gmail.com>
2024-11-17 20:59:38 -0600

    feat: added test for starting black pawn moves
    Need to test both white and black pawn moves
    at start

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -12,5 +12,15 @@ describe Pawn do
         expect(start_white_pawn_e2.possible_moves).to include([5, 3], [4, 3], [5, 2], [5, 4])
       end
     end
+    context "at the start of the game for black" do
+      subject(:start_black_pawn_a6) { described_class.new(:black, [1, 0]) }
+      subject(:start_black_pawn_e6) { described_class.new(:black, [1, 3]) }
+      it "creates list of possible moves" do
+        start_black_pawn_a6.create_possible_moves
+        start_black_pawn_e6.create_possible_moves
+        expect(start_black_pawn_a6.possible_moves).to include([2, 0], [3, 0], [2, 1])
+        expect(start_black_pawn_e6.possible_moves).to include([2, 3], [3, 3], [2, 2], [2, 4])
+      end
+    end
   end
 end
