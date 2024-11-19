386f4cc17c4bc5a6c56ff75c38db73ab49f1cbae
7minutech <elijohnson1107@gmail.com>
2024-11-18 21:36:53 -0600

    feat: added tests for more cases for pawns
    Needs to test cases when a pawn can capture or
    when a piece is blocking its path

--- spec/piece_move_spec.rb
+++ spec/piece_move_spec.rb
@@ -23,5 +23,36 @@ describe PieceMove do
         expect(e2.possible_moves).to contain_exactly([5, 4], [4, 4])
       end
     end
+    context "when a capture is possible" do
+      it "removes impossible moves for pawns" do
+        board = piece_move.board_obj.board
+        d3_black_pawn = Pawn.new(:black, [5, 3])
+        e2 = board[6][4]
+        board[5][3] = d3_black_pawn
+        e2.create_possible_moves
+        piece_move.remove_impossible_pawn_moves(6, 4)
+        expect(e2.possible_moves).to contain_exactly([5, 4], [4, 4], [5, 3])
+      end
+    end
+    context "when a piece is blocking its path one square up" do
+      it "removes impossible moves for pawns" do
+        board = piece_move.board_obj.board
+        board[5][4] = Pawn.new
+        e2 = board[6][4]
+        e2.create_possible_moves
+        piece_move.remove_impossible_pawn_moves(6, 4)
+        expect(e2.possible_moves).to be_empty
+      end
+    end
+    context "when a piece is blocking its path two squares up" do
+      it "removes impossible moves for pawns" do
+        board = piece_move.board_obj.board
+        board[6][4] = Pawn.new
+        e2 = board[6][4]
+        e2.create_possible_moves
+        piece_move.remove_impossible_pawn_moves(6, 4)
+        expect(e2.possible_moves).to contain_exactly([5, 4])
+      end
+    end
   end
 end
