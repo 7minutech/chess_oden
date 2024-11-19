c0f3fc75f6bbaa9762d44dbb18ebfdc054ca98ba
7minutech <elijohnson1107@gmail.com>
2024-11-18 18:46:07 -0600

    feat: added test for edge case
    Need to test for when knight is on the edge of
    the board for to ensure impossible moves aren't
    created

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -4,10 +4,17 @@ describe Knight do
   describe "#create_possible_moves" do
     context "when knight is in the middle of the board" do
       subject(:d4_white_knight) { described_class.new(:white, [4, 3]) }
-      xit "creates list of possible moves" do
+      it "creates list of possible moves" do
         d4_white_knight.create_possible_moves
         expect(d4_white_knight.possible_moves).to include([2, 2], [6, 2], [2, 4], [6, 4],
                                                           [3, 1], [5, 1], [3, 5], [5, 5])
+      end
+    end
+    context "when knight is on edge of board" do
+      subject(:a1_white_knight) { described_class.new(:white, [7, 0]) }
+      it "creates list of possible moves" do
+        a1_white_knight.create_possible_moves
+        expect(a1_white_knight.possible_moves).to contain_exactly([5, 1], [6, 2])
       end
     end
   end
