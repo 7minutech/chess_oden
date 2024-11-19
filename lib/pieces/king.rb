a4d02895dbc2b6de81ff73f0f5f6f109bcdaed63
7minutech <elijohnson1107@gmail.com>
2024-11-18 19:18:52 -0600

    feat: passed for creating possible moves
    Need to add methods for creating moves going
    in all directions

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,10 +1,11 @@
 class King
-  attr_accessor :color, :current_square
+  attr_accessor :color, :current_square, :possible_moves
 
   def initialize(color = nil, current_square = nil)
     @color = color
     @current_square = current_square
     @starting_square = current_square
+    @possible_moves = []
   end
 
   def to_s
@@ -13,6 +14,43 @@ class King
     else
       "\u265A"
     end
+  end
+
+  def create_possible_moves
+    current_row = @current_square[0]
+    current_col = @current_square[1]
+    create_up_moves(current_row, current_col)
+    create_down_moves(current_row, current_col)
+    create_right_moves(current_row, current_col)
+    create_left_moves(current_row, current_col)
+  end
+
+  def create_up_moves(current_row, current_col)
+    return unless current_row.positive?
+
+    @possible_moves.push([current_row - 1, current_col])
+    @possible_moves.push([current_row - 1, current_col + 1]) if current_col < 7
+    @possible_moves.push([current_row - 1, current_col - 1]) if current_col.positive?
+  end
+
+  def create_down_moves(current_row, current_col)
+    return unless current_row < 7
+
+    @possible_moves.push([current_row + 1, current_col])
+    @possible_moves.push([current_row + 1, current_col + 1]) if current_col < 7
+    @possible_moves.push([current_row + 1, current_col - 1]) if current_col.positive?
+  end
+
+  def create_left_moves(current_row, current_col)
+    return unless current_col.positive?
+
+    @possible_moves.push([current_row, current_col - 1])
+  end
+
+  def create_right_moves(current_row, current_col)
+    return unless current_col < 7
+
+    @possible_moves.push([current_row, current_col + 1])
   end
 
   def self.starting_range?(row, col)
