ed3a083651eb1802133b544921fcf9133afd92a0
7minutech <elijohnson1107@gmail.com>
2024-11-27 14:24:57 -0600

    fix: matcher was include now contain exactly
    Should change or could give false positive
    i.e., there could more moves that are invalid than
    those listed

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -8,8 +8,8 @@ describe Pawn do
       it "creates list of possible moves" do
         start_white_pawn_a2.create_possible_moves
         start_white_pawn_e2.create_possible_moves
-        expect(start_white_pawn_a2.possible_moves).to include([5, 0], [4, 0], [5, 1])
-        expect(start_white_pawn_e2.possible_moves).to include([5, 3], [4, 3], [5, 2], [5, 4])
+        expect(start_white_pawn_a2.possible_moves).to contain_exactly([5, 0], [4, 0], [5, 1])
+        expect(start_white_pawn_e2.possible_moves).to contain_exactly([5, 3], [4, 3], [5, 2], [5, 4])
       end
     end
     context "at the start of the game for black" do
@@ -18,8 +18,8 @@ describe Pawn do
       it "creates list of possible moves" do
         start_black_pawn_a6.create_possible_moves
         start_black_pawn_e6.create_possible_moves
-        expect(start_black_pawn_a6.possible_moves).to include([2, 0], [3, 0], [2, 1])
-        expect(start_black_pawn_e6.possible_moves).to include([2, 3], [3, 3], [2, 2], [2, 4])
+        expect(start_black_pawn_a6.possible_moves).to contain_exactly([2, 0], [3, 0], [2, 1])
+        expect(start_black_pawn_e6.possible_moves).to contain_exactly([2, 3], [3, 3], [2, 2], [2, 4])
       end
     end
   end
