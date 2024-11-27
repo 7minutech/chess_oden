ae423c0ea6fb7fe07b2cf27d09978d42a7b5b731
7minutech <elijohnson1107@gmail.com>
2024-11-26 21:49:23 -0600

    feat: passed test for impossible bishop at start
    Added method to remove impossible moves for
    bishops

--- lib/piece_move.rb
+++ lib/piece_move.rb
@@ -44,4 +44,52 @@ class PieceMove
       piece.possible_moves.delete([move[0], move[1]]) unless square != " " && square.color != piece.color
     end
   end
+
+  def remove_impossible_bishop_moves(row, col)
+    piece = @board[row][col]
+    temp_row = row
+    temp_col = col
+    while (temp_row - 1).between?(0, 7) && (temp_col - 1).between?(0, 7) && @board[temp_row - 1][temp_col - 1] == " "
+      temp_row -= 1
+      temp_col -= 1
+    end
+    while (temp_row - 1).between?(0, 7) && (temp_col - 1).between?(0, 7)
+      temp_row -= 1
+      temp_col -= 1
+      piece.possible_moves.delete([temp_row, temp_col])
+    end
+    temp_row = row
+    temp_col = col
+    while (temp_row - 1).between?(0, 7) && (temp_col + 1).between?(0, 7) && @board[temp_row - 1][temp_col + 1] == " "
+      temp_row -= 1
+      temp_col += 1
+    end
+    while (temp_row - 1).between?(0, 7) && (temp_col + 1).between?(0, 7)
+      temp_row -= 1
+      temp_col += 1
+      piece.possible_moves.delete([temp_row, temp_col])
+    end
+    temp_row = row
+    temp_col = col
+    while (temp_row + 1).between?(0, 7) && (temp_col - 1).between?(0, 7) && @board[temp_row + 1][temp_col - 1] == " "
+      temp_row += 1
+      temp_col -= 1
+    end
+    while (temp_row + 1).between?(0, 7) && (temp_col - 1).between?(0, 7)
+      temp_row += 1
+      temp_col -= 1
+      piece.possible_moves.delete([temp_row, temp_col])
+    end
+    temp_row = row
+    temp_col = col
+    while (temp_row + 1).between?(0, 7) && (temp_col + 1).between?(0, 7) && @board[temp_row + 1][temp_col + 1] == " "
+      temp_row += 1
+      temp_col += 1
+    end
+    while (temp_row + 1).between?(0, 7) && (temp_col + 1).between?(0, 7)
+      temp_row += 1
+      temp_col += 1
+      piece.possible_moves.delete([temp_row, temp_col])
+    end
+  end
 end
