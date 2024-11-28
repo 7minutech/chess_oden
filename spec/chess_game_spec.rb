8547dc429f2f88fc4e4e4a23e1ce66b635819caf
7minutech <elijohnson1107@gmail.com>
2024-11-28 14:24:04 -0600

    feat: added test for new method valid_piece
    Must tests that the method to select the square of
    the piece the player wants to move in valid

--- spec/chess_game_spec.rb
+++ spec/chess_game_spec.rb
@@ -2,6 +2,22 @@ require_relative "../lib/chess_game"
 
 describe ChessGame do
   let(:game) { described_class }
+  describe "#valid_piece" do
+    context "when a valid square is entered for white" do
+      before do
+        valid_move1 = "e2"
+        valid_move2 = "Ra1"
+        allow(game).to receive(:gets).and_return(valid_move1, valid_move2)
+      end
+      it "returns no error message" do
+        prompt_message = "Select a piece: "
+        error_message = "That is not a valid square, Please select a valid square"
+        allow(game).to receive(:puts).with(prompt_message).twice
+        expect(game).not_to receive(:puts).with(error_message)
+        game.send(:valid_piece).twice
+      end
+    end
+  end
   describe "#valid_move" do
     context "when a valid move is entered for white" do
       before do
@@ -12,8 +28,8 @@ describe ChessGame do
       it "returns no error message" do
         prompt_message = "Enter your move: "
         error_message = "That is not a valid move please enter a valid"
-        allow(:game).to receive(:puts).with(prompt_message).twice
-        expect(:game).not_to receive(:puts).with(error_message)
+        allow(game).to receive(:puts).with(prompt_message).twice
+        expect(game).not_to receive(:puts).with(error_message)
         game.send(:valid_move).twice
       end
     end
