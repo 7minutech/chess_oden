13b24a989aba02cc396261b1fd921def6e3800d1
7minutech <elijohnson1107@gmail.com>
2024-11-28 14:40:29 -0600

    feat: added test for valid_piece?
    When a player selects their piece move it must be
    a valid piece i.e not the opposite and an piece
    object

--- spec/chess_game_spec.rb
+++ spec/chess_game_spec.rb
@@ -1,20 +1,40 @@
 require_relative "../lib/chess_game"
 
 describe ChessGame do
-  let(:game) { described_class }
-  describe "#valid_piece" do
+  let(:game) { described_class.new }
+  describe "#valid_piece?" do
     context "when a valid square is entered for white" do
-      before do
+      it "returns true" do
         valid_move1 = "e2"
-        valid_move2 = "Ra1"
-        allow(game).to receive(:gets).and_return(valid_move1, valid_move2)
+        valid_move2 = "a1"
+        expect(game.valid_piece?(valid_move1)).to be true
+        expect(game.valid_piece?(valid_move2)).to be true
       end
-      it "returns no error message" do
-        prompt_message = "Select a piece: "
-        error_message = "That is not a valid square, Please select a valid square"
-        allow(game).to receive(:puts).with(prompt_message).twice
-        expect(game).not_to receive(:puts).with(error_message)
-        game.send(:valid_piece).twice
+    end
+    context "when a invalid square is entered for white" do
+      it "returns true" do
+        valid_move1 = "e4"
+        valid_move2 = "a4"
+        expect(game.valid_piece?(valid_move1)).to be false
+        expect(game.valid_piece?(valid_move2)).to be false
+      end
+    end
+    context "when a valid square is entered for black" do
+      it "returns true" do
+        valid_move1 = "f8"
+        valid_move2 = "h7"
+        game.send(:flip_player_turn)
+        expect(game.valid_piece?(valid_move1)).to be true
+        expect(game.valid_piece?(valid_move2)).to be true
+      end
+    end
+    context "when a invalid square is entered for black" do
+      it "returns true" do
+        valid_move1 = "f4"
+        valid_move2 = "h2"
+        game.send(:flip_player_turn)
+        expect(game.valid_piece?(valid_move1)).to be false
+        expect(game.valid_piece?(valid_move2)).to be false
       end
     end
   end
